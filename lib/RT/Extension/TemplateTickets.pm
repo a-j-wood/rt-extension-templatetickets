50daabd769c1fdeff3e87055f1dc0261a8235627
Andrew Wood <ivarch657@gmail.com>
2021-09-09 20:34:25 +0100

    First part of initial skeleton code

--- lib/RT/Extension/TemplateTickets.pm
+++ lib/RT/Extension/TemplateTickets.pm
@@ -12,8 +12,28 @@ RT::Extension::TemplateTickets - Designate tickets as templates for new tickets
 =head1 DESCRIPTION
 
 This extension allows a Request Tracker administrator to mark any ticket as
-a I<template ticket>.  Template tickets can be used when creating a new
-ticket, to pre-populate the ticket creation form.
+a I<template ticket>, and optionally restrict access to specific groups.
+
+When a queue has template tickets which are visible to the current user, a
+drop-down list of available templates will be shown at the top of the ticket
+creation form.  Choosing one of these will redisplay the form with the
+template values pre-populated.
+
+Tickets which have been created from a template will optionally show the
+name of the template they were created from under their I<Basics> section,
+if the template is configured to enable this.
+
+When a template ticket is used to create a new ticket, if the template had
+child tickets, the creation of the new ticket will also trigger automatic
+creation of child tickets patterned after those of the template's children.
+
+For each template ticket, the administrator chooses which of the ticket's
+properties will be included in the template, whether to include child
+tickets, and which child ticket properties to carry over.
+
+Template tickets are administered under I<Admin> - I<Queues>; after choosing
+a queue, go to I<Templates> - I<Tickets> in the page menu.  Administrators
+must have the new B<ShowTicketTemplate> or B<ModifyTicketTemplate> rights.
 
 =head1 RT VERSION
 
@@ -45,6 +65,10 @@ Add this line:
 
 =back
 
+=head1 TUTORIAL
+
+TODO: writeme
+
 =head1 AUTHOR
 
 Andrew Wood
@@ -69,5 +93,10 @@ This is free software, licensed under:
   The GNU General Public License, Version 2, June 1991
 
 =cut
+
+RT::Queue->AddRight(
+    'Admin' => 'ShowTicketTemplate' => 'View ticket templates' );    # loc
+RT::Queue->AddRight(
+    'Admin' => 'ModifyTicketTemplate' => 'Modify ticket templates' );    # loc
 
 1;
